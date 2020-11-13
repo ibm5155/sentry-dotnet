e149392718b24ea4960573916367f6221896336d
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-11-13 12:07:28 -0300

    rollback build props, changed build.ps1

--- build.ps1
+++ build.ps1
@@ -8,5 +8,9 @@ dotnet test -c Release -l $testLogger `
     /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
+msbuild /restore /p:Configuration=Release .\test\Sentry.Tests.uwp\Sentry.Tests.uwp.csproj 
+if ($LASTEXITCODE -ne 0) { exit 1 }
+
+
 dotnet pack -c Release --no-build /p:ContinuousIntegrationBuild=true
 if ($LASTEXITCODE -ne 0) { exit 1 }
