fb250fd92563b14e044cb061d38bcdc48b2e305b
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-11-13 12:19:05 -0300

    blank pr

--- build.ps1
+++ build.ps1
@@ -2,13 +2,13 @@ $ErrorActionPreference = "Stop"
 
 $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
 
+msbuild /restore /p:Configuration=Release .\test\Sentry.Tests.uwp\Sentry.Tests.uwp.csproj 
+if ($LASTEXITCODE -ne 0) { exit 1 }
+
 dotnet test -c Release -l $testLogger `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
     /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\"""'
-if ($LASTEXITCODE -ne 0) { exit 1 }
-
-msbuild /restore /p:Configuration=Release .\test\Sentry.Tests.uwp\Sentry.Tests.uwp.csproj 
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 
